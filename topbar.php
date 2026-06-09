860b4cfaf6a8a8c95be6aca65ccb349064ecc863
Alessandro Benelli <108730040+Tiltaments@users.noreply.github.com>
2026-06-09 19:13:54 +0200

    Fix HTML entities and formatting in topbar.php

--- topbar.php
+++ topbar.php
@@ -1,12 +1,12 @@
 <?php if (session_status() == PHP_SESSION_NONE) session_start(); ?>
 <div id="topbar">
     <?php if (isset($_SESSION['user_id'])): ?>
-        Benvenuto,&nbsp <strong><?php echo htmlspecialchars($_SESSION['user_nome']); ?> &nbsp </strong> |
-        <a href="area_personale.php">I Miei Acquisti</a> &nbsp |
+        Benvenuto,&nbsp; <strong><?php echo htmlspecialchars($_SESSION['user_nome']); ?> &nbsp; </strong> |
+        <a href="area_personale.php">I Miei Acquisti</a> &nbsp; |
         <a href="logout.php" style="color: #e74c3c;">Logout</a>
     <?php else: ?>
-        <a href="login.php">Login</a> &nbsp | <a href="registrazione.php">Registrati</a>
+        <a href="login.php">Login</a> &nbsp; | <a href="registrazione.php">Registrati</a>
     <?php endif; ?>
 
-    &nbsp | <a href="carrello.php">🛒 Carrello (<?php echo isset($_SESSION['carrello']) ? count($_SESSION['carrello']) : 0; ?>)</a>
+    &nbsp; | <a href="carrello.php">🛒 Carrello (<?php echo isset($_SESSION['carrello']) ? count($_SESSION['carrello']) : 0; ?>)</a>
 </div>
